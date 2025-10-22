48749da437c123003b631e933cb896aa823f6a37
GuillermoEAC <guillermochina12@gmail.com>
2025-10-22 13:01:18 -0700

    Unoooo

---
+++ src/pages/mainPage.jsx
@@ -0,0 +1,17 @@
+// src/pages/mainPage.jsx
+import React from "react";
+
+const Mainpage = () => {
+  return (
+    <div>
+      <main className="p-8">
+        <h1 className="text-3xl font-bold">Contenido Principal de la Página</h1>
+        {/* <Header />
+        <Body />
+        <Footer /> */}
+      </main>
+    </div>
+  );
+};
+
+export default Mainpage;
